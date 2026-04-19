4f2b7e071d316f1524d9c03bc109127051b712d1
Sagar Dulal <133942125+SagarDulal09@users.noreply.github.com>
2026-04-19 18:52:47 +0545

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,14 +1,15 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxBnJA-bySZumgVenlMExqcVZUsUSdo3Efxywx_lHL6pFeNXQ6Uxg4bUz2fwKYw9ohJaw/exec"; 
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxBnJA-bySZumgVenlMExqcVZUsUSdo3Efxywx_lHL6pFeNXQ6Uxg4bUz2fwKYw9ohJaw/exec";
 
 async function apiRequest(data) {
     try {
-        const response = await fetch(API_URL, {
+        const response = await fetch(SCRIPT_URL, {
             method: 'POST',
             body: JSON.stringify(data)
         });
-        return await response.json();
-    } catch (error) {
-        console.error("API Error:", error);
-        return { success: false };
+        const result = await response.json();
+        return result;
+    } catch (err) {
+        console.error("API Error:", err);
+        return { success: false, message: "Network error. Check connection." };
     }
 }
